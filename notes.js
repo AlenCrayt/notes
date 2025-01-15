d107496223e6f69a5bba48c72a39133ab18385ab
AlenCrayt <agus.santisteban@gmail.com>
2025-01-15 15:26:30 -0300

    Added more console.log lines

--- notes.js
+++ notes.js
@@ -1,16 +1,19 @@
 //have a user system with each user having it's own set of notes
 const http = require("http");
 const fs = require("fs");
+let parsed_html;
 
 fs.readFile("./pages/index.html", (err, data) => {
   if (err != null) {
     console.log(err);
   }
+  console.log(data);
   console.log("index.html read correctly");
 });
 
 const server = http.createServer((req, resp) => {
   if (req.method == "GET") {
+    console.log("receiving GET request");
     resp.writeHead(200, { "Content-Type": "text/html" });
     resp.write("./pages/index.html");
   }
