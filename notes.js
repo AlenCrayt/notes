dff7b81cd5d909a2ae5a34854a771c02fe474f37
AlenCrayt <agus.santisteban@gmail.com>
2025-01-15 15:42:37 -0300

    Changed if arguments inside of createServer

--- notes.js
+++ notes.js
@@ -1,22 +1,18 @@
 //have a user system with each user having it's own set of notes
 const http = require("http");
 const fs = require("fs");
-let parsed_html;
-
-fs.readFile("./pages/index.html", (err, data) => {
-  if (err != null) {
-    console.log(err);
-  }
-  //console.log(data);
-  parsed_html = data;
-  console.log("index.html read correctly");
-});
 
 const server = http.createServer((req, resp) => {
-  if (req.method == "GET") {
-    console.log("receiving GET request");
+  if (req.url == "/") {
+    fs.readFile("./pages/index.html", (err, data) => {
+      if (err != null) {
+        console.log(err);
+      }
+      //console.log(data);
+      console.log("index.html read correctly");
+    });
     resp.writeHead(200, { "Content-Type": "text/html" });
-    resp.write(parsed_html);
+    resp.end(data);
   }
 });
 server.listen(8080);
