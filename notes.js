59219061606b0b91a61fe3bf86dfa308caf34db8
AlenCrayt <agus.santisteban@gmail.com>
2025-01-20 15:44:27 -0300

    Added tailwindcss classes, made a basic log in menu

--- notes.js
+++ notes.js
@@ -5,13 +5,16 @@ const fs = require("fs");
 const server = http.createServer((req, resp) => {
   if (req.url == "/") {
     fs.readFile("./pages/index.html", (err, data) => {
-      if (err != null) {
+      if (err) {
         console.log(err);
+        resp.writeHead(500, { "Content-Type": "text/plain" });
+        resp.end("Internal Server Error");
+      } else {
+        //console.log(data);
+        console.log("index.html read correctly");
+        resp.writeHead(200, { "Content-Type": "text/html" });
+        resp.end(data);
       }
-      //console.log(data);
-      console.log("index.html read correctly");
-      resp.writeHead(200, { "Content-Type": "text/html" });
-      resp.end(data);
     });
   }
 });
